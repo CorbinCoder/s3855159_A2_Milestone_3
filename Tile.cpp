eb404c5cb3e4c756480cdb2bb6a31e916a93c535
s3965243 <s3965243@student.rmit.edu.au>
2024-07-23 15:08:06 +1000

    Tile Class function definitions
    Just gotta fill in the functions now

--- Tile.cpp
+++ Tile.cpp
@@ -1,4 +1,32 @@
-
 #include "Tile.h"
 
-// Empty... for now?
+Tile::Tile(Colour colour, Shape shape)
+{
+    this->colour = colour;
+    this->shape = shape;
+}
+
+// deconstuctor
+Tile::~Tile()
+{
+}
+
+// Copy Constructor
+Tile::Tile(Tile &other)
+{
+}
+
+// Move Constructor
+Tile::Tile(Tile &&other)
+{
+}
+
+Colour Tile::getColour()
+{
+    return this->colour;
+}
+
+Shape Tile::getShape()
+{
+    return this->shape;
+}
--- Tile.h
+++ Tile.h
@@ -8,10 +8,27 @@ typedef char Colour;
 // Define a Shape type
 typedef int Shape;
 
-class Tile {
+class Tile
+{
 public:
    Colour colour;
-   Shape  shape;
+   Shape shape;
+
+   // constructor
+   Tile(Colour colour, Shape shape);
+
+   // Deconstructor
+   ~Tile();
+
+   // Copy Constructor
+   Tile(Tile &other);
+
+   // Move Constructor
+   Tile(Tile &&other);
+
+   Colour getColour();
+
+   Shape getShape();
 };
 
 #endif // ASSIGN2_TILE_H
