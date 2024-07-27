e9392aa4965f0d1ea3740a1feb26d89018ef0ae4
CorbinCoder <s3855159@student.rmit.edu.au>
2024-07-28 01:41:52 +1000

    Added Hand class

---
+++ Hand.cpp
@@ -0,0 +1,59 @@
+#include "Hand.h"
+
+// Default constructor
+Hand::Hand(int handsize)
+{
+    this->handsize = handsize;
+}
+
+// Copy constructor
+Hand::Hand(Hand &other)
+{
+    this->tiles = other.tiles;
+}
+
+// Destructor
+Hand::~Hand()
+{
+    tiles.clear();
+}
+
+// Methods
+
+
+// Get/Set
+Tile* Hand::getTile(Colour colour, Shape shape)
+{
+    for (int i = 0; i < sizeof(this->tiles); i++)
+    {
+        if (this->tiles[i].getColour() == colour
+        && this->tiles[i].getShape() == shape)
+        {
+            return &this->tiles[i];
+        }
+    }
+}
+
+void Hand::setTile(Colour colour, Shape shape, Tile* tile)
+{
+    for (int i = 0; i < sizeof(this->tiles); i++)
+    {
+        if (this->tiles.at(i).getColour() == colour
+        && this->tiles.at(i).getShape() == shape)
+        {
+            // TODO
+            // Assign new tile to position
+            // this->tiles.at(i) = tile;
+        }
+    }
+}
+
+int Hand::getHandsize()
+{
+    return this->handsize;
+}
+
+void Hand::setHandsize(int handsize)
+{
+    this->handsize = handsize;
+}
